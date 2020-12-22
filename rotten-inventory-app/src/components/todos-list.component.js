a9aeb2200b8dd6a6aea7b05ee570063bfc63dcf3
Papa-Sean <smsugrue@gmail.com>
2020-12-21 22:46:54 -0500

    need to add edit

--- rotten-inventory-app/src/components/todos-list.component.js
+++ rotten-inventory-app/src/components/todos-list.component.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const Todo = props =>(
+const Todo = props => (
     <tr>
         <td>{props.todo.todo_description}</td>
         <td>{props.todo.todo_responsible}</td>
         <td>{props.todo.todo_priority}</td>
         <td>
-            <Link to={"/edit/" + props.todo._id}></Link>
+            <Link to={"/edit/"+props.todo._id}>Edit</Link>
         </td>
     </tr>
 )
